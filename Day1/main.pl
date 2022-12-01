e7eb9d5e761909cf41b43d87a2fc4594f5f93d41
EJSteen <e.j.steenbergen@student.utwente.nl>
2022-12-01 16:23:34 +0100

    Update main.pl

--- Day1/main.pl
+++ Day1/main.pl
@@ -1,30 +1,49 @@
 :- use_module(library(lists)).
 :- use_module(library(pio)).
 
+
+
+% Get values from data and put in a list with sublists
 lines([]) --> call(eos), !.
 lines([Line | Lines]) --> line(Line), lines(Lines).
 
+% End of stream
 eos([],[]).
 
+% Make sublists of every inventory
 line([]) --> ("\n" ; call(eos)), !.
-line([Val | Vals]) --> [Val], line(Vals).
+line([Inv | Invs]) --> [Inv], line(Invs).
 
+% Creates the counter and the list of the totals
 counts(List, Counts) :- counts(List, 0, [], Counts).
 
-counts([], Count, Inc, Results) :- reverse([Count | Inc], Results).
-counts([[]|Vals], Count, Inc, Results) :- counts(Vals, 0, [Count | Inc], Results).
-counts([Val | Vals], Count, Inc, Results) :-
-    string_codes(Val, String),
+% puts the inventories in the right order
+counts([], Count, Totals, Results).
+
+% Begins a new inventory and adds the total to the totals list
+counts([[]|Invs], Count, Totals, Results) :- counts(Invs, 0, [Count | Totals], Results).
+
+% Sums up the values of an inventory
+counts([Inv | Invs], Count, Totals, Results) :-
+    string_codes(Inv, String),
     number_string(Num, String),
     NewCount is Count + Num,
-    counts(Vals, NewCount, Inc, Results).
+    counts(Invs, NewCount, Totals, Results).
 
+
+% Get the highest total of calories in an inventory
 solve(Counts, Results) :- max_list(Counts, Results).
 
+% Get the sum of the three highest total of calories in an inventory
 solve2(Counts, Results) :-
     sort(Counts, SortedCounts),
-    reverse(SortedCounts, [A, B, C| X]),
+    reverse(SortedCounts, [A, B, C | _]),
     sum_list([A,B,C], Results).
 
 
-:- phrase_from_file(lines(Lines), 'C:/.SSHome/NotSchool/AOC/AdventOfCode2022/Day1/data.txt'), counts(Lines, Counts), solve(Counts, Results1), writeln(Results1), solve2(Counts, Results2), writeln(Results2).
+:- phrase_from_file(lines(Lines), 'C:/.SSHome/NotSchool/AOC/AdventOfCode2022/Day1/data.txt'), 
+counts(Lines, Counts), 
+solve(Counts, Results1), 
+writeln(Results1), 
+solve2(Counts, Results2), 
+writeln(Results2).
